6f9559a64ffc1661fde8027af5c21a1d0af63559
Thainá <thaina.mariano@alunos.unis.edu.br>
2026-05-24 13:26:19 -0300

    Corrige preflight CORS

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GameController;
 
+Route::options('/{any}', function () {
+    return response()->json([], 200);
+})->where('any', '.*');
+
 Route::post('/iniciar-jogo', [GameController::class, 'startGame']);
 
 Route::post('/validar-tentativa', [GameController::class, 'validateGuess']);
